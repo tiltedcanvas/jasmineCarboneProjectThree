7ccb1c5b498b52bdca5bba5db6bdb965e182f856
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-11-16 15:07:09 -0500

    added song options:notes:

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const songOptions = {
+	songs: [
+		{
+            songTitle: 'Juice',
+            songOrder: 'songOne',
+		},
+		{	
+            songTitle: 'Soulmate',
+            songOrder: 'songTwo',
+		},
+		{	
+            songTitle: 'Truth Hurts',
+            songOrder: 'songThree',
+        },
+        {
+            songTitle: 'Good As Hell',
+            songOrder: 'songFour',
+        },
+        {
+            songTitle: 'Cuz I Love U',
+            songOrder: 'songFive',
+        }
+	]
+};
+
+//document ready
+$(function(){
+
+});
+console.log('ready');
+
