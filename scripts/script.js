4143c2f99396e6a51a43edc96466fed79fbe6572
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-11-17 16:59:03 -0500

    quiz is functionally working:boom:

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,127 @@
-const songOptions = {
-    songOne: {
+const songOptions = [
+    {
+        id:'songOne',
         score: 0,
-        // 'Juice'
+        label: 'Juice'
     },
-    songTwo: {
+    {       
+        id:'songTwo',
         score: 0,
-        // 'Soulmate',
+        label: 'Soulmate'
     },
-    songThree: {
+    {        
+        id:'songThree',
         score: 0,
-        // 'Truth Hurts',
+        label: 'Truth Hurts'
     },
-    songFour: {
+    {        
+        id:'songFour',
         score: 0,
-        // 'Good As Hell',
+        label: 'Good As Hell'
     },
-    songFive: {
+
+    {        
+        id:'songFive',
         score: 0,
-        // 'Cuz I Love U'
-    }
-}
+        label: 'Cuz I Love U'
+    },
+]
+
 function updateScore(selectedAnswer) {
-    songOptions[selectedAnswer.id].score++
+    selectedOption = songOptions.find(function(opt) {
+        return opt.id === selectedAnswer.id
+    })
+
+    // If selectedOption is not undefined, update the score
+    if(selectedOption) {
+        selectedOption.score++;
+    }
+
 }
 
 function sortSongOptions (a, b) {
-    return  a.value > b.value ? 1 : -1
+    return  a.score > b.score ? -1: 1;
+}
+
+function onNextClick(e) {
+    const isAnswerSelected = $(e.target).parent().find('input:checked').length > 0 
+    // Parents is different because it goes up more than one level
+    const parentSection = $(e.target).parents('section')
+    //  $(e.target).parent().find('input:checked') returns an array
+    // If that array has a length of 0, nothing was selected
+    if (isAnswerSelected) {
+        updateScore($(e.target).parent().find('input:checked')[0])
+        if(parentSection.next().length > 0) {
+            $(document).scrollTop(parentSection.next().offset().top)
+        }
+    } 
+    else {
+        alert("You need to select an option!");
+    }
+}
+function displayResult(winner) {
+    const popup = $('.results')
+    console.log(popup)
+    const popupText = popup.find('h3');
+    popupText.text(`You are ${winner.label}!`)
+    popup.show();
+}
+// on restart click map through the array of songOptions 
+// set every score to = 0 
+// using jq get an array of inputs that are :checked
+// map through them to uncheck them and then animate the body position to top
+// hide results
+
+
+function onRestartClick() {
+    const popup = $('.results')
+    popup.hide();
+
+    for (let i = 0; i < songOptions.length; i++) {
+        const song = songOptions[i];
+        song.score = 0;
+    } 
+
+    const selectedInputs = $('body').find('input:checked')
+    for (let i = 0; i < selectedInputs.length; i++) {
+        const input = selectedInputs[i];
+        $(input).prop("checked", false);
+    }
+    $(document).scrollTop(0);
 }
 
 
-// $('form').on('submit', function(e) {
-//     e.preventDefault();
-// });
+
 
 //document ready
 $(function(){
+    $('.startQuiz').on('click', function(e) {
+        e.preventDefault();
+
+        const header = $(e.target).parents('header')
+        console.log(header)
+        $(document).scrollTop(header.next().offset().top)
+    })
+
     $('.nextQuestion').on('click', function(e) {
         e.preventDefault();
-        //  $(e.target).parent().find('input:checked') returns an array
-        // If that array has a length of 0, nothing was selected
-        const isAnswerSelected = $(e.target).parent().find('input:checked').length > 0 
+        onNextClick(e)
 
-        if (isAnswerSelected) {
-            updateScore($(e.target).parent().find('input:checked')[0])
-        } 
+        
     })
+
+
     $('.finishQuiz').on('click', function(e) {
         e.preventDefault();
-        
-        console.log(songOptions.sort(sortSongOptions))
-        
-        
-    })
 
+        onNextClick(e)
+        const sortedOptions = songOptions.sort(sortSongOptions);
+        displayResult(sortedOptions[0]);
+    })
+    $('.restart').on('click', function(e) {
+        e.preventDefault();
+        onRestartClick()
+    })
 
     // const answer = true;
 
